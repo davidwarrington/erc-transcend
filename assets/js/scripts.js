acea88c119b79d359caed61d0ed0ac6f0f96ff14
davidwarrington <davidmwarrington+github@gmail.com>
2018-03-07 16:17:24 +0000

    Push temp pathname disable live

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,22 +16,19 @@
 //# sourceMappingURL=bootstrap.min.js.map
 $(document).ready(function () {
 
-    if (window.location.pathname === '/') {
+    // if (window.location.pathname === '/') {
         var site_intro = {
             body: $('body'),
             el: $('.site-intro'),
             lower: $('.site-intro--lower'),
             upper: $('.site-intro--upper'),
         };
-
-        console.log(true);
         
         site_intro.lower.on('animationend', function () {
-            console.log('animation over');
             site_intro.el.hide();
             site_intro.body.removeClass('no-scroll');
         });
-    }
+    // }
 
     if ($('.portfolio-item--desc').length) {
         // $('.portfolio-item--details').hover(function (e) {
